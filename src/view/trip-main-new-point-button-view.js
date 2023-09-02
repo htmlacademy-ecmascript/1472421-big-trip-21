89454812d802bd0e5e8f86debda32cfb2a2ed22d
exert777 <alter0910@mail.ru>
2023-09-02 14:48:04 +0300

    Сделал view заглушки, если список точек пуст

--- src/view/trip-main-new-point-button-view.js
+++ src/view/trip-main-new-point-button-view.js
@@ -3,11 +3,10 @@ import AbstractView from '../framework/view/abstract-view.js';
 
 function createNewPointButton() {
   return `
-    <button class="trip-main__event-add-btn  btn  btn--big  btn--yellow" type="button" disabled>New event</button>
+    <button class="trip-main__event-add-btn  btn  btn--big  btn--yellow" type="button">New event</button>
   `;
 }
 
-/* Компонент, отвечающий за раздел информации о путешествии в блоке trip-main */
 export default class NewPointButton extends AbstractView {
 
   constructor(){
